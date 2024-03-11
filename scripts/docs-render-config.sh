7447745bc87de2a0d23031ff5ec5294fa7966cba
Andrei Sorokin <Andrei_Sorokin2@epam.com>
2024-03-11 09:13:13 +0100

    renamed placeholders

--- scripts/docs-render-config.sh
+++ scripts/docs-render-config.sh
@@ -6,10 +6,10 @@
 
 lang=$1
 
-sed "s/LANG_PLACEHOLDER/$lang/g" docs/mkdocs.yml > docs/_mkdocs.yml
+sed "s/LANGUAGE/$lang/g" docs/mkdocs.yml > docs/_mkdocs.yml
 if [ $lang = "en" ]; then
     # place English to the root of the site
-    sed -i "" "s/SITE_PLACEHOLDER//g" docs/_mkdocs.yml
+    sed -i "" "s/SITE_PREFIX//g" docs/_mkdocs.yml
 else
-    sed -i "" "s/SITE_PLACEHOLDER/$lang/g" docs/_mkdocs.yml
+    sed -i "" "s/SITE_PREFIX/$lang/g" docs/_mkdocs.yml
 fi
